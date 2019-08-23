4f67fbbd75bbf11a3e27c9e742f2e3c834d0d1a1
Cliff Saringe <saringecliff@gmail.com>
2019-08-23 10:57:12 +0300

    Page layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Pages/Home";
+import Default from "./Pages/Default";
+import Recipe from "./Pages/Recipe";
+import SingleRecipe from "./Pages/SingleRecipe";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div >
-    Hello from App 
-    </div>
+    <Router>
+      <main>
+        <Navbar></Navbar>
+      <Switch>
+       <Route path="/" exact component = {Home}></Route>
+       <Route path="/recipes" exact component = {Recipe}></Route>
+       <Route path="/recipes/:id" component = {SingleRecipe}></Route>
+       <Route component = {Default}></Route>
+    
+     </Switch>
+
+
+      </main>
+      
+    </Router>
+   
+   
   );
 }
 
---
+++ src/Pages/Recipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Recipe extends Component {
+    render() {
+        return (
+            <h4>
+                Hello from Recipe
+            </h4>
+        )
+    }
+}
